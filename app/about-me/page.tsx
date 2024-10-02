dc582bda0cf6c5d2b429498c43ce070816e13d6d
Hoon Jeong <iamhoonjeong@gmail.com>
2024-10-02 13:20:30 -0700

    Adjust elements heights

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -41,8 +41,8 @@ export default function AboutMe() {
   return (
     <main className="overflow-hidden animate-fadeIn text-center">
       <Header categoryOrder="category2" title="ABOUT ME" />
-      <section className="w-full h-sectionWithoutHeaderMobile sm:h-sectionWithoutHeaderDesktop flex flex-col items-center bg-category3">
-        <div className="mt-8 sm:mt-6 flex">
+      <section className="w-full h-sectionWithoutHeaderMobile sm:h-sectionWithoutHeaderDesktop flex flex-col items-center bg-category3 overflow-hidden">
+        <div className="mt-8 sm:mt-6 flex z-10">
           <Link href={'https://www.linkedin.com/in/iamhoonjeong/'} rel="noopener noreferrer" target="_blank">
             <Image className="w-10 mr-3" src="/images/icon-linkedin.svg" width={0} height={0} alt="linkedin icon" />
           </Link>
@@ -50,27 +50,27 @@ export default function AboutMe() {
             <Image className="w-10" src="/images/icon-mail.svg" width={0} height={0} alt="email icon" />
           </Link>
         </div>
-        <div className="text-2xl sm:text-4xl text-category2 font-black mt-4 sm:mt-6">Hi, This is</div>
+        <div className="text-2xl sm:text-4xl text-category2 font-black mt-4 sm:mt-6 z-10">Hi, This is</div>
         <Image
-          className="w-52 sm:w-72 mt-4 sm:mt-6 animate-sectionTitleShaking"
+          className="w-52 sm:w-72 mt-4 sm:mt-6 animate-sectionTitleShaking z-10"
           src="/images/title-my-name.svg"
           width={100}
           height={100}
           alt="title image"
         />
-        <div className="text-2xl sm:text-4xl text-white font-black mt-4 sm:mt-6">
+        <div className="text-2xl sm:text-4xl text-white font-black mt-4 sm:mt-6 z-10">
           <div>{`I'm working`}</div>
           <div>{`as`}</div>
         </div>
-        <div className="text-2xl sm:text-4xl font-black text-category2 mt-2 sm:mt-4">
+        <div className="text-2xl sm:text-4xl font-black text-category2 mt-2 sm:mt-4 z-10">
           <div>{`Software Engineer`}</div>
           <div>{`Front-End Developer`}</div>
           <div>{`and also, Designer`}</div>
         </div>
         <div
           ref={circleRef}
-          style={{ top: circlePositionTop, width: '75dvw', height: '75dvw', maxWidth: '400px', maxHeight: '400px' }}
-          className="absolute flex bg-category4 rounded-full overflow-hidden z-10"
+          style={{ top: circlePositionTop, width: '75dvw', height: '75dvw', maxWidth: '45dvh', maxHeight: '45dvh' }}
+          className="absolute flex bg-category4 rounded-full overflow-hidden"
         >
           <Image
             ref={imageInsideCircle}
@@ -98,13 +98,13 @@ export default function AboutMe() {
           />
         </div>
       </section>
-      <section className="h-dvh flex flex-col justify-end items-center bg-category4 pb-14">
-        <div className="text-2xl sm:text-4xl font-black text-category1 mt-2 mb-6 sm:mt-4">
+      <section className="h-dvh flex flex-col justify-end items-center bg-category4 pb-14 overflow-hidden">
+        <div className="text-2xl sm:text-4xl font-black text-category1 mt-2 mb-6 sm:mt-4 z-10">
           <div>{`I always consider`}</div>
           <div>{`UserFlow, Creative,`}</div>
           <div>{`Cutting-Edge Technology`}</div>
         </div>
-        <div className="w-4/5 h-1/2 max-w-2xl relative">
+        <div className="w-4/5 h-1/2 max-w-2xl relative z-10">
           <Image
             style={{ width: '100px', height: '100px', left: '-50px', top: '-50px' }}
             className="w-full h-full absolute animate-bounce"
